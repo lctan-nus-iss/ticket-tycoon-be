b65d9a787b5928c136e91687615ddbec631a10aa
Tan Lee Chiang <lctan@nus.edu.sg>
2026-06-16 10:25:12 +0800

    commit  minor change

--- src/main/java/com/tickertycoon/port/LlmPort.java
+++ src/main/java/com/tickertycoon/port/LlmPort.java
@@ -6,5 +6,11 @@ public interface LlmPort {
 
     LlmResponse complete(LlmRequest request);
 
+    /**
+     * Streams the model output incrementally for the given request.
+     *
+     * @param request the prompt and options used to generate the response
+     * @return a {@link Flux} emitting response chunks in order until completion
+     */
     Flux<String> stream(LlmRequest request);
 }
